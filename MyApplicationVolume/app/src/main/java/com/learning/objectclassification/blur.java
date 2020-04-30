36a17c6b3ad49b4d00c83d8971aa2aaf1371f83c
Computer-HuiMa <2270937002@qq.com>
2020-04-30 13:02:59 +0800

    add objectclassification

---
+++ MyApplicationVolume/app/src/main/java/com/learning/objectclassification/blur.java
@@ -0,0 +1,41 @@
+package com.learning.objectclassification;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+public class blur {
+    /*
+     * 使用RenderScript实现高斯模糊的算法
+     */
+    public Bitmap blur(Bitmap bitmap){
+        Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        RenderScript rs = RenderScript.create(getApplicationContext());
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
+
+        Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
+
+        blurScript.setRadius(20.0f);
+
+        blurScript.setInput(allIn);
+        blurScript.forEach(allOut);
+        allOut.copyTo(outBitmap);
+        bitmap.recycle();
+
+        rs.destroy();
+
+        return outBitmap;
+
+    }
+
+    private Context getApplicationContext() {
+    }
+
+
+}
