d9d16ef5cbb2416db33211ee152743b85814984f
yjz980120 <1009450080@qq.com>
2020-05-18 23:59:11 +0800

    camera

--- MyApplicationVolume/app/src/main/java/com/example/myapplication__volume/MainActivity.java
+++ MyApplicationVolume/app/src/main/java/com/example/myapplication__volume/MainActivity.java
@@ -17,6 +17,8 @@ import android.content.IntentFilter;
 import android.content.pm.ConfigurationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.opengl.GLSurfaceView;
@@ -54,6 +56,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import com.example.basic.FileManager;
 import com.example.basic.Image4DSimple;
@@ -79,13 +82,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -128,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean ifLoadLocal = false;
     private boolean ifRemote = false;
     private boolean ifDownloadByHttp = false;
+//    private boolean ifTakePhoto = false;
 
     private boolean[] temp_mode = new boolean[5];
 
@@ -174,11 +181,17 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private static int REQUEST_PERMISSION_CODE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private static final int REQUEST_TAKE_PHOTO = 3;
 
     //    private int Paintmode = 0;
     private ArrayList<Float> lineDrawed = new ArrayList<Float>();
 
     private BroadcastReceiver broadcastReceiver;
+
+    private String currentPhotoPath; //指定一个不会跟其他文件产生冲突的文件名，用于后面相机拍照的图片的保存
+
+    private File showPic;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -508,6 +521,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            System.out.println("BBBBB");
+//            Bundle extras = data.getExtras();
+//
+////            Uri photouri = (Uri) extras.get(MediaStore.EXTRA_OUTPUT);
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");  //如果直接保存imageBitmap，保存成txt? 还是直接保存图片？
+//            savePhoto(imageBitmap);
+//            ImageView imageView = new ImageView(this);
+//            imageView.setImageBitmap(imageBitmap);
+////            ifTakePhoto = false;
+//            return;
+            Bitmap bitmap= BitmapFactory.decodeFile(String.valueOf(showPic));
+            return;
+        }
 
         if (resultCode == RESULT_OK) {
             String fodlerPath = data.getDataString();
@@ -717,6 +744,16 @@ public class MainActivity extends AppCompatActivity {
                     myrenderer.SetPath(filePath);
                     ifDownloadByHttp = false;
                 }
+
+//                if (ifTakePhoto){
+//                    System.out.println("BBBBB");
+//                    Bundle extras = data.getExtras();
+//                    Bitmap imageBitmap = (Bitmap) extras.get("data");  //如果直接保存imageBitmap，保存成txt? 还是直接保存图片？
+//                    ImageView imageView = new ImageView(this);
+//                    imageView.setImageBitmap(imageBitmap);
+//                    ifTakePhoto = false;
+////                    return;
+//                }
 //
 //                ArrayList<ArrayList<Float>> swc = new ArrayList<ArrayList<Float>>();
 //                SwcReader swcReader = new SwcReader();
@@ -951,9 +988,7 @@ public class MainActivity extends AppCompatActivity {
     private void FileManager(View v) {
         new XPopup.Builder(this)
                 .atView(v)
-
-                .asAttachList(new String[]{"Open Local file", "Open Remote file", "Load SWC file", "Camera"},
-
+                .asAttachList(new String[]{ "Open Local file", "Open Remote file", "Load SWC file" ,"Camera"},
                         new int[]{},
                         new OnSelectListener() {
                             @Override
@@ -969,7 +1004,7 @@ public class MainActivity extends AppCompatActivity {
                                         remote(v);
                                         break;
                                     case "Camera":
-//                                        Camera();
+                                        Camera();
                                         break;
                                 }
 
@@ -977,6 +1012,157 @@ public class MainActivity extends AppCompatActivity {
                         })
                 .show();
     }
+// 不保存完整图片，仅拍照
+//    private void Camera(){
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private String getImageFilePath(){
+        String mCaptureDir = "/storage/emulated/0/C3/cameraPhoto";
+        File dir = new File(mCaptureDir);
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+
+        String mCapturePath = mCaptureDir + "/" + "Photo_" + System.currentTimeMillis() +".jpg";
+//        System.out.println("before" + mCapturePath);
+//        try {
+//            FileOutputStream fos = new FileOutputStream(mCapturePath);
+////            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+////                        fos.flush();
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("after" + mCapturePath);
+        return mCapturePath;
+    }
+
+    private void Camera() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+//            String photoFilePath = null;
+            try {
+                photoFile = createImageFile();
+                showPic = photoFile;
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+//            photoFilePath = getImageFilePath();
+
+//            System.out.println(photoFilePath);
+//            try {
+//                File f = new File(photoFilePath);
+//
+//                if (!f.exists())
+//                    f.createNewFile();
+//            } catch (Exception e){
+//                System.out.println("AAAAAAA");
+//                e.printStackTrace();
+//            }
+            String imageUri = photoFile.getAbsolutePath();
+//            String imageUri = insertImageToSystem(context, photoFilePath);
+
+//            Uri photoURI = Uri.parse(imageUri);
+//            System.out.println(imageUri);
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        "com.example.myapplication__volume.provider",
+                        photoFile);
+
+//                takePictureIntent.putExtra("output", photoURI);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+//                ifTakePhoto = true;
+//                startActivityForResult(takePictureIntent, 1);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                Log.v("Camera", "Here we are");
+            }
+        }
+    }
+
+    private void savePhoto(Bitmap photoBitmap){
+        String photoFilePath = getImageFilePath();
+
+        System.out.println(photoFilePath);
+        File f = new File(photoFilePath);
+        FileOutputStream fileOutputStream = null;
+        try {
+//            File f = new File(photoFilePath);
+
+            fileOutputStream = new FileOutputStream(f);
+            if (photoBitmap != null){
+                if (photoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)) {
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                }
+            }
+
+            else {
+                System.out.println("bitmap is empty");
+            }
+            return;
+        } catch (Exception e){
+            System.out.println("AAAAAAA");
+            e.printStackTrace();
+        }
+
+        if (!f.exists()) {
+            Uri uri = Uri.parse(photoFilePath);
+
+            try {
+                ParcelFileDescriptor parcelFileDescriptor =
+                        getContext().getContentResolver().openFileDescriptor(uri, "w");
+
+                fileOutputStream = new ParcelFileDescriptor.AutoCloseOutputStream(parcelFileDescriptor);
+                if (photoBitmap != null) {
+                    if (photoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)) {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                } else {
+                    System.out.println("bitmap is empty");
+                }
+            } catch (Exception e) {
+                System.out.println("CCCCCCC");
+                e.printStackTrace();
+            }
+        }
+
+        myrenderer.SetPath(photoFilePath);
+
+        myGLSurfaceView.requestRender();
+
+//        String imageUri = photoFilePath;
+////            String imageUri = insertImageToSystem(context, photoFilePath);
+//
+//        Uri photoURI = Uri.parse(imageUri);
+    }
+
 
 
 //    private void Camera() {
@@ -1575,6 +1761,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             url = MediaStore.Images.Media.insertImage(context.getContentResolver(), imagePath, filename, "ScreenShot from C3");
         } catch (FileNotFoundException e) {
+            System.out.println("SSSSSSSSSSSS");
             e.printStackTrace();
         }
         System.out.println("Filename: " + filename);
