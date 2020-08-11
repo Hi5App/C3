b98f4a1710020f4a275b198e093e249072997d6a
zhuyih <zyh97102015@163.com>
2020-08-11 15:17:23 +0800

    add some help information

--- MyApplicationVolume/app/src/main/java/com/example/myapplication__volume/MainActivity.java
+++ MyApplicationVolume/app/src/main/java/com/example/myapplication__volume/MainActivity.java
@@ -4735,7 +4735,7 @@ public class MainActivity extends AppCompatActivity {
         new XPopup.Builder(this)
 
                 .asConfirm("C3: VizAnalyze Big 3D Images", "By Peng lab @ BrainTell. \n\n" +
-                                "Version: 20200810 21:43 UTC+8 build",
+                                "Version: 20200811 15:15 UTC+8 build",
                         new OnConfirmListener() {
                             @Override
                             public void onConfirm() {
