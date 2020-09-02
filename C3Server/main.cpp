3eace3f89461feb2c913b968b5914c980355b921
huanglei <huanglei>
2020-09-02 15:31:41 +0800

    Update main.cpp

--- C3Server/main.cpp
+++ C3Server/main.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 #include <QFileInfo>
 #include <limits>
+#include <array>
 
 void getApo(QString brainDir,QString apoDir);
 void writeBrainInfo(QString apoDir,QString infoWithTxt);
 void getBB(const V_NeuronSWC_list& T,const QString & Filename);
-void writeCheckBrainInfo(QString swcPath,QString infoWithTxt);
+void writeCheckBrainInfo(QString swcPath,QString resInfo,QString resPath);
 void combineData(QString swcPath,QString apoPath,QString dstPath);
 void combineDataWithoutCombine(QString swcPath,QString apoPath,QString dstPath);
 //image dir:put brain image
@@ -20,6 +21,11 @@ void combineDataWithoutCombine(QString swcPath,QString apoPath,QString dstPath);
 //data dir:put anotation data such as swc
 //tmp dir:put some temp file(after use will delete)
 //neuronInfo dir:a whole brain info save as a .txt in it,will send to user.
+struct checkInfo
+{
+    QString brainId;
+    std::array<uint,6> cor;
+};
 
 int main(int argc, char *argv[])
 {
@@ -69,44 +75,7 @@ int main(int argc, char *argv[])
     }
     return a.exec();
 }
-//    qDebug()<<list.size();
 
-
-//        QDir dir("/home/allencenter/Desktop/17302_check");
-//        auto list=dir.entryInfoList(QDir::Files|QDir::NoDotAndDotDot);
-//        for(auto i :list)
-//        {
-//           qDebug()<<i.fileName();
-//            QString swcname=i.absoluteFilePath();
-//            auto nt=readSWC_file(swcname);
-//            V_NeuronSWC_list V_list= NeuronTree__2__V_NeuronSWC_list(nt);
-//            V_NeuronSWC_list T3;
-//            V_NeuronSWC_list T2;
-//            V_NeuronSWC_list others;
-//            for(V_NeuronSWC& seg:V_list.seg)
-//            {
-//                if(seg.row.at(0).type==3)
-//                {
-//                    T3.seg.push_back(seg);
-//                }else if (seg.row.at(0).type==2) {
-//                    T2.seg.push_back(seg);
-//                }else
-//                {
-//                    others.seg.push_back(seg);
-//                }
-//            }
-//            qDebug()<<i.fileName();
-//            qDebug()<<"T2";
-//            getBB(T2,i.baseName());
-//            qDebug()<<"T3";
-//            getBB(T3,i.baseName());
-//            qDebug()<<"other";
-//            getBB(others,i.baseName());
-
-//        }
-//    qDebug()<<sum;
-//    return a.exec();
-//}
 void combineDataWithoutCombine(QString swcPath,QString apoPath,QString dstPath)
 {
     QDir swcDir(swcPath);
@@ -127,23 +96,6 @@ void combineDataWithoutCombine(QString swcPath,QString apoPath,QString dstPath)
             if(nt.listNeuron.size()!=0)
             {
                 enSwcList.push_back(apoBaseName);
-//                {
-//                    auto markers=readAPO_file(apoPath+"/"+apoBaseName+"/"+apoBaseName+".apo");
-//                    {
-//                        dstDir.mkdir(apoBaseName);
-//                        QString tempname =dstPath+"/"+apoBaseName+"/"+apoBaseName+".ano";
-//                        QFile anofile(tempname);
-//                        anofile.open(QIODevice::WriteOnly);
-//                        QString str1="APOFILE="+apoBaseName+".ano.apo";
-//                        QString str2="SWCFILE="+apoBaseName+".ano.eswc";
-
-//                        QTextStream out(&anofile);
-//                        out<<str1<<endl<<str2;
-//                        anofile.close();
-//                    }
-//                    writeAPO_file(dstPath+"/"+apoBaseName+"/"+apoBaseName+".ano.apo",markers);
-//                    writeESWC_file(dstPath+"/"+apoBaseName+"/"+apoBaseName+".ano.eswc",nt);
-//                }
             }else
             {
                 emSwcList.push_back(apoBaseName);
@@ -345,128 +297,190 @@ void writeBrainInfo(QString apoDir,QString infoWithTxt)
 
 }
 
-void writeCheckBrainInfo(QString swcPath,QString infoWithTxt)
+void writeCheckBrainInfo(QString swcPath,QString resInfo,QString resPath)
 {
     QDir dir(swcPath);
     QFileInfoList list=dir.entryInfoList(QDir::Files|QDir::NoDotAndDotDot);
+    QVector<checkInfo> swcList;
+
+    for(int i=0;i<list.count();i++)
+    {
+        QString l;
+        QRegExp swcNameExp("(.*)_(.*)_(.*)_(.*).v3draw_x(.*)_y(.*)_z(.*)_(.*)");
+        if(swcNameExp.indexIn(list[i].baseName())!=-1)
+        {
+            QString brainId=swcNameExp.cap(1).trimmed();
+            uint centerx=swcNameExp.cap(2).toDouble();
+            uint centery=swcNameExp.cap(3).toDouble();
+            uint centerz=swcNameExp.cap(4).toDouble();
+            uint lx=swcNameExp.cap(5).toUInt();
+            uint ly=swcNameExp.cap(6).toUInt();
+            uint lz=swcNameExp.cap(7).toUInt();
+            QString neuronId;
+            QTextStream s(&neuronId);
+            s<<brainId<<"_";
+            s.setFieldWidth(8);
+            s.setPadChar('0');
+            s<<i+1;
+
+            checkInfo info={neuronId,{centerx,centery,centerz,lx,ly,lz}};
+            swcList.push_back(info);
+            writeSWC_file(resPath+"/"+neuronId+".swc",readSWC_file(list[i].absoluteFilePath()));
+        }
+    }
+    QString infoWithTxt="check"+resInfo;
 
     QFile f(infoWithTxt);
-    if(f.open(QIODevice::Append|QIODevice::Text))
+    if(f.open(QIODevice::WriteOnly|QIODevice::Text))
     {
         QTextStream stream(&f);
-        stream<<endl<<endl<<"#Neuron_number:"<<list.count()<<endl;
-        for(int i=0;i<list.count();i++)
+        //读写分辨率的信息
+        QFile resF(resInfo);
+        if(resF.open(QIODevice::ReadOnly|QIODevice::Text))
         {
-            stream<<QString::number(i+1)<<":"<<list[i].baseName()<<endl;
+            stream<<resF.readAll();
+            resF.close();
+
+        }
+        else
+        {
+            qDebug()<<resF.errorString()<<endl;
+            exit(0);
+        }
+        stream<<endl<<endl<<"#Neuron_number:"<<list.count()<<endl;
+        for(int i=0;i<swcList.size();i++)
+        {
+            stream<<QString::number(i+1)<<":"<<swcList[i].brainId<<endl;
         }
         stream<<endl<<"#Neuron Info"<<endl;
-        for(int i=0;i<list.count();i++)
+        for(int i=0;i<swcList.size();i++)
         {
-            auto nt=readSWC_file(list[i].absoluteFilePath());
-            if(nt.listNeuron.size()==0)
-            {
-                qDebug()<<"error:"<<list[i].fileName();
-                continue;
-            }
-            for(int j=0;j<nt.listNeuron.size();j++)
-            {
-                if(nt.listNeuron[j].pn==-1)
-                {
-                    stream<<"##"<<list[i].baseName()<<endl
-                         <<"soma:"<<QString::number(int(nt.listNeuron[j].x))
-                         <<";"<<QString::number(int(nt.listNeuron[j].y))
-                         <<";"<<QString::number(int(nt.listNeuron[j].z))<<endl;
-                           break;
-                }
-            }
-
-            V_NeuronSWC_list V_list= NeuronTree__2__V_NeuronSWC_list(nt);
-            V_NeuronSWC_list T3;
-            V_NeuronSWC_list T2;
-            V_NeuronSWC_list others;
-            for(V_NeuronSWC& seg:V_list.seg)
-            {
-                if(seg.row.at(0).type==3)
-                {
-                    T3.seg.push_back(seg);
-                }else if (seg.row.at(0).type==2) {
-                    T2.seg.push_back(seg);
-                }else
-                {
-                    others.seg.push_back(seg);
-                }
-            }
-
-            int arborN=0;
-            if(T3.seg.size()!=0)
-            {
-                arborN++;
-
-            }
-            if(T2.seg.size()!=0)
-            {
-
-                arborN++;
-            }
-            if(others.seg.size()!=0)
-            {
-                arborN++;
-            }
-
-            int cnt=1;
-            stream<<"arbor:"<<QString::number(arborN)<<endl;
-            if(T3.seg.size()!=0)
-            {
-                auto nt3=V_NeuronSWC_list__2__NeuronTree(T3);
-                auto v3=getBB(T3);
-                for(int i=0;i<nt3.listNeuron.size();i++)
-                {
-                    if(nt3.listNeuron[i].pn==-1)
-                    {
-                            stream<<QString::number(cnt++)<<":"<<QString::number(int(nt3.listNeuron[i].x))<<";"<<QString::number(int(nt3.listNeuron[i].y))
-                                 <<";"<<QString::number(int(nt3.listNeuron[i].z))<<";"<<QString::number(v3.at(0))<<";"<<QString::number(v3.at(1))
-                                <<";"<<QString::number(v3.at(2))<<";"<<QString::number(v3.at(3))
-                                <<";"<<QString::number(v3.at(4))<<";"<<QString::number(v3.at(5))<<endl;
-
-                               break;
-                    }
-                }
-            }
-            if(T2.seg.size()!=0)
-            {
-                auto nt2=V_NeuronSWC_list__2__NeuronTree(T2);
-                auto v2=getBB(T2);
-                for(int i=0;i<nt2.listNeuron.size();i++)
-                {
-                    if(nt2.listNeuron[i].pn==-1)
-                    {
-                            stream<<QString::number(cnt++)<<":"<<QString::number(int(nt2.listNeuron[i].x))<<";"<<QString::number(int(nt2.listNeuron[i].y))
-                                 <<";"<<QString::number(int(nt2.listNeuron[i].z))<<";"<<QString::number(v2.at(0))<<";"<<QString::number(v2.at(1))
-                                <<";"<<QString::number(v2.at(2))<<";"<<QString::number(v2.at(3))
-                                <<";"<<QString::number(v2.at(4))<<";"<<QString::number(v2.at(5))<<endl;
-
-                               break;
-                    }
-                }
-            }
-            if(others.seg.size()!=0)
-            {
-                auto ntother=V_NeuronSWC_list__2__NeuronTree(others);
-                auto vo=getBB(others);
-                for(int i=0;i<ntother.listNeuron.size();i++)
-                {
-                    if(ntother.listNeuron[i].pn==-1)
-                    {
-                            stream<<QString::number(cnt++)<<":"<<QString::number(int(ntother.listNeuron[i].x))<<";"<<QString::number(int(ntother.listNeuron[i].y))
-                                 <<";"<<QString::number(int(ntother.listNeuron[i].z))<<";"<<QString::number(vo.at(0))<<";"<<QString::number(vo.at(1))
-                                <<";"<<QString::number(vo.at(2))<<";"<<QString::number(vo.at(3))
-                                <<";"<<QString::number(vo.at(4))<<";"<<QString::number(vo.at(5))<<endl;
-
-                               break;
-                    }
-                }
-            }
-            }
+            stream<<"##"<<swcList[i].brainId<<endl
+                 <<"soma:"<<0<<";"<<0<<";"<<0<<endl;
+                        stream<<"arbor:"<<1<<endl;
+            stream<<1<<":"<<swcList[i].cor[0]*2<<";"<<swcList[i].cor[1]*2<<swcList[i].cor[2]*2<<endl;
         }
+
+    }
+
+
+//    QFile f(infoWithTxt);
+//    if(f.open(QIODevice::Append|QIODevice::Text))
+//    {
+//
+//        stream<<endl<<endl<<"#Neuron_number:"<<list.count()<<endl;
+//        for(int i=0;i<list.count();i++)
+//        {
+//            stream<<QString::number(i+1)<<":"<<list[i].baseName()<<endl;
+//        }
+//        stream<<endl<<"#Neuron Info"<<endl;
+//        for(int i=0;i<list.count();i++)
+//        {
+//            auto nt=readSWC_file(list[i].absoluteFilePath());
+//            if(nt.listNeuron.size()==0)
+//            {
+//                qDebug()<<"error:"<<list[i].fileName();
+//                continue;
+//            }
+//            for(int j=0;j<nt.listNeuron.size();j++)
+//            {
+//                if(nt.listNeuron[j].pn==-1)
+//                {
+//                    stream<<"##"<<list[i].baseName()<<endl
+//                         <<"soma:"<<QString::number(int(nt.listNeuron[j].x))
+//                         <<";"<<QString::number(int(nt.listNeuron[j].y))
+//                         <<";"<<QString::number(int(nt.listNeuron[j].z))<<endl;
+//                           break;
+//                }
+//            }
+
+//            V_NeuronSWC_list V_list= NeuronTree__2__V_NeuronSWC_list(nt);
+//            V_NeuronSWC_list T3;
+//            V_NeuronSWC_list T2;
+//            V_NeuronSWC_list others;
+//            for(V_NeuronSWC& seg:V_list.seg)
+//            {
+//                if(seg.row.at(0).type==3)
+//                {
+//                    T3.seg.push_back(seg);
+//                }else if (seg.row.at(0).type==2) {
+//                    T2.seg.push_back(seg);
+//                }else
+//                {
+//                    others.seg.push_back(seg);
+//                }
+//            }
+
+//            int arborN=0;
+//            if(T3.seg.size()!=0)
+//            {
+//                arborN++;
+
+//            }
+//            if(T2.seg.size()!=0)
+//            {
+
+//                arborN++;
+//            }
+//            if(others.seg.size()!=0)
+//            {
+//                arborN++;
+//            }
+
+//            int cnt=1;
+//            stream<<"arbor:"<<QString::number(arborN)<<endl;
+//            if(T3.seg.size()!=0)
+//            {
+//                auto nt3=V_NeuronSWC_list__2__NeuronTree(T3);
+//                auto v3=getBB(T3);
+//                for(int i=0;i<nt3.listNeuron.size();i++)
+//                {
+//                    if(nt3.listNeuron[i].pn==-1)
+//                    {
+//                            stream<<QString::number(cnt++)<<":"<<QString::number(int(nt3.listNeuron[i].x))<<";"<<QString::number(int(nt3.listNeuron[i].y))
+//                                 <<";"<<QString::number(int(nt3.listNeuron[i].z))<<";"<<QString::number(v3.at(0))<<";"<<QString::number(v3.at(1))
+//                                <<";"<<QString::number(v3.at(2))<<";"<<QString::number(v3.at(3))
+//                                <<";"<<QString::number(v3.at(4))<<";"<<QString::number(v3.at(5))<<endl;
+
+//                               break;
+//                    }
+//                }
+//            }
+//            if(T2.seg.size()!=0)
+//            {
+//                auto nt2=V_NeuronSWC_list__2__NeuronTree(T2);
+//                auto v2=getBB(T2);
+//                for(int i=0;i<nt2.listNeuron.size();i++)
+//                {
+//                    if(nt2.listNeuron[i].pn==-1)
+//                    {
+//                            stream<<QString::number(cnt++)<<":"<<QString::number(int(nt2.listNeuron[i].x))<<";"<<QString::number(int(nt2.listNeuron[i].y))
+//                                 <<";"<<QString::number(int(nt2.listNeuron[i].z))<<";"<<QString::number(v2.at(0))<<";"<<QString::number(v2.at(1))
+//                                <<";"<<QString::number(v2.at(2))<<";"<<QString::number(v2.at(3))
+//                                <<";"<<QString::number(v2.at(4))<<";"<<QString::number(v2.at(5))<<endl;
+
+//                               break;
+//                    }
+//                }
+//            }
+//            if(others.seg.size()!=0)
+//            {
+//                auto ntother=V_NeuronSWC_list__2__NeuronTree(others);
+//                auto vo=getBB(others);
+//                for(int i=0;i<ntother.listNeuron.size();i++)
+//                {
+//                    if(ntother.listNeuron[i].pn==-1)
+//                    {
+//                            stream<<QString::number(cnt++)<<":"<<QString::number(int(ntother.listNeuron[i].x))<<";"<<QString::number(int(ntother.listNeuron[i].y))
+//                                 <<";"<<QString::number(int(ntother.listNeuron[i].z))<<";"<<QString::number(vo.at(0))<<";"<<QString::number(vo.at(1))
+//                                <<";"<<QString::number(vo.at(2))<<";"<<QString::number(vo.at(3))
+//                                <<";"<<QString::number(vo.at(4))<<";"<<QString::number(vo.at(5))<<endl;
+
+//                               break;
+//                    }
+//                }
+//            }
+//            }
+//        }
 }
 
