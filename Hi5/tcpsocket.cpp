e46632f0314b11dfc233cde8254e148f5081de76
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2021-01-30 20:53:23 +0800

    modify tcp class

--- Hi5/tcpsocket.cpp
+++ Hi5/tcpsocket.cpp
@@ -2,18 +2,16 @@
 #include <QCoreApplication>
 #include <QDir>
 #include <iostream>
-TcpSocket::TcpSocket(QObject *parent) : QObject(parent)
-{
-
-}
-
-void TcpSocket::processMsg(QString msg)
-{
-
-}
+extern QString ip;
+extern QString port;
 
 void TcpSocket::sendMsg(QString str)
 {
+    if(socket.state()!=QAbstractSocket::ConnectedState&&!connectHost(ip,port))
+    {
+        unconnected();
+    }
+
     const QString data=str+"\n";
     int datalength=data.size();
     QString header=QString("DataTypeWithSize:%1 %2\n").arg(0).arg(datalength);
@@ -24,6 +22,11 @@ void TcpSocket::sendMsg(QString str)
 
 void TcpSocket::sendFiles(QStringList filepaths)
 {
+    if(socket.state()!=QAbstractSocket::ConnectedState&&!connectHost(ip,port))
+    {
+        unconnected();
+    }
+
     for(auto filepath:filepaths)
     {
         QString filename=filepath.section('/',-1);
@@ -34,11 +37,11 @@ void TcpSocket::sendFiles(QStringList filepaths)
             continue;
         }
         QByteArray fileData=f.readAll();
+        f.close();
         QString header=QString("DataTypeWithSize:%1 %2 %3\n").arg(1).arg(filename).arg(fileData.size());
         this->socket.write(header.toStdString().c_str(),header.size());
         this->socket.write(fileData,fileData.size());
         this->socket.flush();
-        f.close();
     }
 }
 
@@ -48,39 +51,12 @@ void TcpSocket::onread()
     {
         if(datatype.datasize==0)
         {
-            if(socket.canReadLine())
+            if(socket.bytesAvailable()>=1024||socket.canReadLine())
             {
                 //read head
-                QString msg=socket.readLine(100);
-                if(!msg.endsWith('\n'))
-                {
-                    emit errorprocess(1);
-                    return;
-                }else
-                {
-                    msg=msg.trimmed();
-                    if(msg.startsWith("DataTypeWithSize:"))
-                    {
-                        msg=msg.right(msg.size()-QString("DataTypeWithSize:").size());
-                        QStringList paras=msg.split(' ',QString::SkipEmptyParts);
-                        if(paras.size()==2&&paras[0]=="0")
-                        {
-                            datatype.datasize=paras[1].toInt();
-                        }else if(paras.size()==3&&paras[0]=="1")
-                        {
-                            datatype.isFile=true;
-                            datatype.datasize=paras[2].toInt();
-                            datatype.filename=paras[1];
-                        }else
-                        {
-                            errorprocess(2,msg);
-                        }
-                    }else
-                    {
-                        errorprocess(3,msg);
-                    }
+                QString msg=socket.readLine(1024);
+                if(processHeader(msg))
                     onread();
-                }
             }
         }else
         {
@@ -88,38 +64,37 @@ void TcpSocket::onread()
             if(socket.bytesAvailable()>=datatype.datasize)
             {
                 QString msg=socket.readLine(datatype.datasize);
-                if(!msg.endsWith('\n'))
+                if(processMsg(msg))
                 {
-                    errorprocess(1);
-                    return;
-                }else
-                {
-                    msg=msg.trimmed();
-                    processMsg(msg);
-                    resetDataType();
                     onread();
                 }
             }
         }
-    }else{
-        if(socket.bytesAvailable()>=datatype.datasize)
+    }else if(socket.bytesAvailable())
+    {
+        int ret = 0;
+        auto bytes=socket.read(datatype.datasize);
+        if(datatype.f->write(bytes)==bytes.size())
         {
+            datatype.datasize-=bytes.size();
+            if(datatype.datasize==0)
             {
-                if(!QDir(QCoreApplication::applicationDirPath()+"/download").exists())
-                    QDir(QCoreApplication::applicationDirPath()).mkdir("download");
-                QString filePath=QCoreApplication::applicationDirPath()+"/download/"+datatype.filename;
-                QFile file(filePath);
-                file.open(QIODevice::WriteOnly);
-                int length=file.write(socket.read(datatype.datasize));
-                if(length!=datatype.datasize)
-                {
-                    qDebug()<<"Error:read file";
-                }
-                file.flush();
-                file.close();
+                datatype.f->close();
+                delete datatype.f;
+                datatype.f=nullptr;
+                resetDataType();
+                processFile(datatype.f->fileName());
+                onread();
+            }else if(datatype.datasize<0)
+            {
+                ret = 6;
             }
-            resetDataType();
-            onread();
+        }else{
+            ret = 5;
+        }
+        if(ret!=0)
+        {
+            errorprocess(ret,datatype.f->fileName());
         }
     }
 }
@@ -129,20 +104,120 @@ void TcpSocket::resetDataType()
     datatype.isFile=false;
     datatype.datasize=0;
     datatype.filename.clear();
+    if(datatype.f)
+    {
+        delete datatype.f;
+        datatype.f=nullptr;
+    }
+
 }
 
+bool TcpSocket::processMsg(const QString msg)
+{
+    if(msg.endsWith('\n')) return true;
+    else {
+        errorprocess(1,msg);
+    }
+}
+
+bool TcpSocket::connectHost(QString ip,QString port)
+{
+    socket.connectToHost(ip,port.toUInt());
+    return socket.waitForConnected(30000);
+}
+
+bool TcpSocket::processHeader(const QString rmsg)
+{
+    int ret = 0;
+    if(rmsg.endsWith('\n'))
+    {
+        QString msg=rmsg.trimmed();
+        if(msg.startsWith("DataTypeWithSize:"))
+        {
+            msg=msg.right(msg.size()-QString("DataTypeWithSize:").size());
+            QStringList paras=msg.split(";;",QString::SkipEmptyParts);
+            if(paras.size()==2&&paras[0]=="0")
+            {
+                datatype.datasize=paras[1].toInt();
+            }else if(paras.size()==3&&paras[0]=="1")
+            {
+                datatype.isFile=true;
+                datatype.filename=paras[1];
+                datatype.datasize=paras[2].toInt();
+                if(!QDir(QCoreApplication::applicationDirPath()+"/tmp").exists())
+                    QDir(QCoreApplication::applicationDirPath()).mkdir("tmp");
+                QString filePath=QCoreApplication::applicationDirPath()+"/tmp/"+datatype.filename;
+                datatype.f=new QFile(filePath);
+                if(!datatype.f->open(QIODevice::WriteOnly))
+                    ret=4;
+            }else
+            {
+                ret=3;
+            }
+        }else
+        {
+            ret = 2;
+        }
+    }else
+    {
+        ret = 1;
+    }
+    if(!ret) return true;
+    errorprocess(ret,rmsg.trimmed()); return false;
+}
 
 void TcpSocket::errorprocess(int errcode,QString msg)
 {
+    //errcode
+    //1:not end with '\n';
+    //2:not start wth "DataTypeWithSize"
+    //3:msg not 2/3 paras
+    //4:cannot open file
+    //5:read socket != write file
+    //6:next read size < 0
     if(errcode==1)
     {
-        std::cout<<"ERROR:msg not end with '\n',We will disconnect the socket."<<std::endl;
+        std::cerr<<"ERROR:msg not end with '\n',";
     }else if(errcode==2)
     {
-        std::cout<<QString("ERROR:%1 not format error,We will disconnect the socket.").arg(msg).toStdString().c_str()<<std::endl;
+        std::cerr<<QString("ERROR:%1 not start wth \"DataTypeWithSize\"").arg(msg).toStdString().c_str();
     }else if(errcode==3)
     {
-        std::cout<<QString("ERROR:%1 not start with DataTypeWithSize:,We will disconnect the socket.").arg(msg).toStdString().c_str()<<std::endl;
+        std::cerr<<QString("ERROR:%1 not 2/3 paras").arg(msg).toStdString().c_str();
+    }else if(errcode==4){
+        std::cerr<<QString("ERROR:%1 cannot open file").arg(msg).toStdString().c_str();
+    }else if(errcode==5){
+        std::cerr<<QString("ERROR:%1 read socket != write file").arg(msg).toStdString().c_str();
+    }else if(errcode==6){
+        std::cerr<<QString("ERROR:%1 next read size < 0").arg(msg).toStdString().c_str();
     }
+    std::cerr<<",We will disconnect the socket\n";
     this->socket.disconnectFromHost();
 }
+
+
+bool ManageSocket::processFile(const QString filepath)
+{
+    return true;
+}
+
+bool ManageSocket::processMsg(const QString msg)
+{
+    if(TcpSocket::processMsg(msg)==false) return false;
+    return true;
+}
+
+
+bool MessageSocket::processFile(const QString filepath)
+{
+return true;
+}
+
+bool MessageSocket::processMsg(const QString msg)
+{
+    if(TcpSocket::processMsg(msg)==false) return false;
+    return true;
+}
+
+
+
--- Hi5/tcpsocket.h
+++ Hi5/tcpsocket.h
@@ -2,7 +2,8 @@
 #define TCPSOCKET_H
 
 #include <QTcpSocket>
-
+#include <QFile>
+#include <QMessageBox>
 class TcpSocket : public QObject
 {
     Q_OBJECT
@@ -10,23 +11,64 @@ class TcpSocket : public QObject
         bool isFile=false;//false msg,true file
         qint64 datasize=0;
         QString filename;
+        QFile *f=nullptr;
     };
 public:
+    explicit TcpSocket(QObject *parent = nullptr):QObject(parent)
+    {
+        connect(&socket,&QTcpSocket::disconnected,this,&TcpSocket::disconnected);
+    }
+    virtual ~TcpSocket()=default;
 
-    explicit TcpSocket(QObject *parent = nullptr);
+    virtual bool processMsg(const QString);
+    virtual bool processFile(const QString)=0;
+
     void sendMsg(QString str);
     void sendFiles(QStringList filepath);
+
 public slots:
     void onread();
 private:
+    void resetDataType();
+    bool connectHost(QString ip,QString port);
+    bool processHeader(const QString msg);
+    void errorprocess(int,QString msg="");
+
     QTcpSocket socket;
     DataType datatype;
-    void resetDataType();
-    void errorprocess(int,QString msg="");
-    void processMsg(QString);
 signals:
-
-
+    //for extern
+    void unconnected();
+    void disconnected();
 };
+
+class ManageSocket:public TcpSocket
+{
+    Q_OBJECT
+public:
+    explicit ManageSocket(QObject *parent=nullptr):TcpSocket(parent){}
+    bool processMsg(const QString);
+    bool processFile(const QString);
+    ~ManageSocket()=default;
+public slots:
+
+signals:
+     void signalLoginACK(QString ackmsg);
+     void signalRegisterACK(QString ackmsg);
+     void signalForgetACK(QString ackmsg);
+};
+
+class MessageSocket:public TcpSocket
+{
+    Q_OBJECT
+    explicit MessageSocket(QObject *parent=nullptr) :TcpSocket(parent){}
+     bool processMsg(const QString);
+     bool processFile(const QString);
+    ~MessageSocket()=default;
+};
+
+
+
+
 
 #endif // TCPSOCKET_H
