34921193109e863181825180ea98200229ad1711
xfqqq <761496766@qq.com>
2021-11-29 19:54:07 +0800

    reorganize code of HomeActivity

--- MyApplicationVolume/app/src/main/java/com/penglab/hi5/core/ui/home/screens/HomeActivity.java
+++ MyApplicationVolume/app/src/main/java/com/penglab/hi5/core/ui/home/screens/HomeActivity.java
@@ -3,21 +3,23 @@ package com.penglab.hi5.core.ui.home.screens;
 import static com.penglab.hi5.core.MainActivity.ifGuestLogin;
 import static com.penglab.hi5.core.Myapplication.ToastEasy;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.legacy.app.ActionBarDrawerToggle;
 import androidx.viewpager.widget.ViewPager;;
 
 import com.gigamole.navigationtabstrip.NavigationTabStrip;
@@ -33,7 +35,7 @@ import com.penglab.hi5.core.ui.login.LoginActivity;
 import com.penglab.hi5.core.ui.home.adapters.MainPagerAdapter;
 import com.penglab.hi5.dataStore.PreferenceLogin;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "HomeActivity";
     private static final String USERNAME = "USERNAME";
@@ -41,130 +43,145 @@ public class HomeActivity extends AppCompatActivity {
     public static String username = null;
     private long exitTime = 0;
 
-    private int mSelectedItem = 0;
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
     private Context homeContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        homeContext = this;
 
+        homeContext = this;
         username = getIntent().getStringExtra(USERNAME);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        Toolbar toolbar = findViewById(R.id.toolbar_home);
         setSupportActionBar(toolbar);
 
-        drawerLayout  = findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.drawable.ic_menu,R.string.drawer_open_content_description,R.string.drawer_closed_content_description);
-        drawerLayout.setDrawerListener(mDrawerToggle);
-        drawerLayout.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        setNavDrawer();
+
+        // set up for cards view
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_main);
+        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+        viewPager.setOffscreenPageLimit(2);
+
+        // set up for the tab
+        final NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts);
+//        navigationTabStrip.setTitles("HOW WE WORK", "WE WORK WITH");
+        navigationTabStrip.setTitles("HOW WE WORK");
+        navigationTabStrip.setViewPager(viewPager);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setNavDrawer(){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.drawer_open_content_description,
+                R.string.drawer_closed_content_description) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 super.onDrawerSlide(drawerView, slideOffset);
                 drawerLayout.bringChildToFront(drawerView);
                 drawerLayout.requestLayout();
             }
-        });
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        }
+        };
 
-        final NavigationView navigationView = findViewById(R.id.nav_view);
-        if (ifGuestLogin){
+        drawerLayout.addDrawerListener(drawerToggle);
+        navigationView = findViewById(R.id.nav_view);
+        if (ifGuestLogin) {
             MenuItem accountItem = navigationView.getMenu().findItem(R.id.nav_account);
             accountItem.setTitle("Login");
         }
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                mSelectedItem = item.getItemId();
-                switch(item.getItemId()){
-                    case R.id.nav_account:
-                        if(ifGuestLogin){
-                            login();
-                        }else
-                            logout();
-                        break;
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    @SuppressLint("NonConstantResourceId")
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_account:
+                if (ifGuestLogin) {
+                    login();
+                } else
+                    logout();
+                break;
 //                    case R.id.nav_settings:
 //                        Settings();
 //
 ////                        setSettings();
 //                        break;
-                    case R.id.nav_help:
-                        try {
-                            Intent helpIntent = new Intent(HomeActivity.this, HelpActivity.class);
-                            startActivity(helpIntent);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    case R.id.nav_About:
-                        About();
-                        break;
-                }
-
-                item.setChecked(true);
-                drawerLayout.closeDrawer(navigationView);
-                return false;
-            }
-        });
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_main);
-        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
-        viewPager.setOffscreenPageLimit(2);
-
-        final NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts);
-//        navigationTabStrip.setTitles("HOW WE WORK", "WE WORK WITH");
-        navigationTabStrip.setTitles("HOW WE WORK");
-        navigationTabStrip.setViewPager(viewPager);
-    }
-        @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
+            case R.id.nav_help:
+                Intent helpIntent = new Intent(HomeActivity.this, HelpActivity.class);
+                startActivity(helpIntent);
+                break;
+            case R.id.nav_About:
+                About();
+                break;
+        }
+        drawerLayout.closeDrawer(navigationView);
+        return false;
     }
 
     /*
-     Start this activity
+    Start this activity
      */
-    public static void start(Context context){
+    public static void start(Context context) {
         start(context, null);
     }
 
-    public static void start(Context context, String username){
+    public static void start(Context context, String username) {
         Intent intent = new Intent(context, HomeActivity.class);
         intent.putExtra(USERNAME, username);
         context.startActivity(intent);
     }
 
     /*
-    Press twice to return
+    Press twice to exit app
      */
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-//            if ((System.currentTimeMillis() - exitTime) > 2000) {
-//                ToastEasy("Press again to exit the program");
-//                exitTime = System.currentTimeMillis();
-//            } else {
-//                finish();
-//            }
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-
-        public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                 drawerLayout.closeDrawers();
                 return false;
-            }else{
+            } else {
                 if ((System.currentTimeMillis() - exitTime) > 2000) {
                     ToastEasy("Press again to exit the program");
                     exitTime = System.currentTimeMillis();
@@ -178,7 +195,7 @@ public class HomeActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    private void logout(){
+    private void logout() {
 
         AlertDialog aDialog = new AlertDialog.Builder(homeContext)
                 .setTitle("Log out")
@@ -194,7 +211,7 @@ public class HomeActivity extends AppCompatActivity {
 //                        AgoraMsgManager.getInstance().getRtmClient().logout(null);
 
                         PreferenceLogin preferenceLogin = new PreferenceLogin(HomeActivity.this);
-                        preferenceLogin.setPref(preferenceLogin.getUsername(),preferenceLogin.getPassword(),false, true);
+                        preferenceLogin.setPref(preferenceLogin.getUsername(), preferenceLogin.getPassword(), false, true);
                         // DemoCache.clear();
 
                         startActivity(new Intent(HomeActivity.this, LoginActivity.class));
@@ -210,7 +227,7 @@ public class HomeActivity extends AppCompatActivity {
         aDialog.show();
     }
 
-    private void login(){
+    private void login() {
 
         AlertDialog aDialog = new AlertDialog.Builder(homeContext)
                 .setTitle("Log in")
@@ -226,7 +243,7 @@ public class HomeActivity extends AppCompatActivity {
 //                        AgoraMsgManager.getInstance().getRtmClient().logout(null);
 
                         PreferenceLogin preferenceLogin = new PreferenceLogin(HomeActivity.this);
-                        preferenceLogin.setPref(preferenceLogin.getUsername(),preferenceLogin.getPassword(),false, true);
+                        preferenceLogin.setPref(preferenceLogin.getUsername(), preferenceLogin.getPassword(), false, true);
                         // DemoCache.clear();
 
                         startActivity(new Intent(HomeActivity.this, LoginActivity.class));
@@ -256,27 +273,6 @@ public class HomeActivity extends AppCompatActivity {
                 .setCancelText("Cancel")
                 .setConfirmText("Confirm")
                 .show();
-    }
-
-
-    /*
-    Create options menu for toolbar
-    */
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.items, menu);
-//        return true;
-//    }
-
-    private void setUpNavDrawer(){
-
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;}
-    return true;
     }
 }
 
