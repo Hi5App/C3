ed9a1e99c85cd3d793633eb3c4cd10b3edd0760e
linglizhang0 <zhangllec@126.com>
2022-03-10 19:27:07 +0800

    finish QualityInspectionDataSource part

--- MyApplicationVolume/app/src/main/java/com/penglab/hi5/data/QualityInspectionDataSource.java
+++ MyApplicationVolume/app/src/main/java/com/penglab/hi5/data/QualityInspectionDataSource.java
@@ -7,7 +7,9 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.penglab.hi5.basic.image.MarkerList;
 import com.penglab.hi5.basic.image.XYZ;
+import com.penglab.hi5.basic.utils.FileHelper;
 import com.penglab.hi5.chat.nim.InfoCache;
+import com.penglab.hi5.core.Myapplication;
 import com.penglab.hi5.core.net.HttpUtilsQualityInspection;
 import com.penglab.hi5.core.net.HttpUtilsSoma;
 import com.penglab.hi5.data.model.img.PotentialArborInfo;
@@ -31,6 +33,7 @@ public class QualityInspectionDataSource {
     private final MutableLiveData<Result> potentialArborLocationResult = new MutableLiveData<>();
     private final MutableLiveData<Result> arborMarkerListResult = new MutableLiveData<>();
     private final MutableLiveData<Result> updateArborMarkerResult = new MutableLiveData<>();
+    private final MutableLiveData<Result> downloadSwcResult = new MutableLiveData<>();
     private String responseData;
 
     public LiveData<Result> getPotentialArborLocationResult() {
@@ -64,7 +67,6 @@ public class QualityInspectionDataSource {
                         try {
                             JSONObject potentialArborLocation = new JSONObject(responseData);
                             JSONObject loc = potentialArborLocation.getJSONObject("loc");
-
                             PotentialArborInfo potentialArborInfo = new PotentialArborInfo(
                                     potentialArborLocation.getInt("id"),
                                     potentialArborLocation.getInt("somaId"),
@@ -97,33 +99,49 @@ public class QualityInspectionDataSource {
         }
     }
 
-    public void getSwc(int x,int y,int z,int size,String arborName){
+    public void getSwc(String res,int offsetX,int offsetY,int offsetZ,int size,String arborName){
         try{
-            JSONObject pa1 = new JSONObject().put("x", x - size/2).put("y", y - size/2).put("z", z - size/2);
-            JSONObject pa2 = new JSONObject().put("x", x + size/2).put("y", y + size/2).put("z", z + size/2);
-            JSONObject bBox = new JSONObject().put("pa1", pa1).put("pa2", pa2).put("res", "").put("obj", arborName);
+            JSONObject pa1 = new JSONObject().put("x", offsetX - size/2).put("y", offsetY - size/2).put("z", offsetZ - size/2);
+            JSONObject pa2 = new JSONObject().put("x", offsetX + size/2).put("y", offsetY + size/2).put("z", offsetZ + size/2);
+            JSONObject bBox = new JSONObject().put("pa1", pa1).put("pa2", pa2).put("res", res).put("obj", arborName);
             JSONObject userInfo = new JSONObject().put("name", InfoCache.getAccount()).put("passwd", InfoCache.getToken());
             HttpUtilsQualityInspection.getSwcWithOkHttp(userInfo,bBox, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
+                    downloadSwcResult.postValue(new Result.Error(new Exception("Connect Failed When Download Music")));
                 }
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
-                    int responseCode = response.code();
-                    if(responseCode == 200){
-                        responseData = response.body().string();
-                        try{
-                            JSONObject arbor_location = new JSONObject(responseData);
-                            JSONObject loc = arbor_location.getJSONObject("loc");
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                    try {
+                        int responseCode = response.code();
+                        if(responseCode == 200){
+                            if(response.body()!= null){
+                                byte[] fileContent = response.body().bytes();
+                                Log.e(TAG, "file size: " + fileContent.length);
+                                Log.e(TAG, "file size: " + fileContent.length);
+                                String storePath = Myapplication.getContext().getExternalFilesDir(null) + "/imagename/somaname";
+                                String filename = arborName + "_" + res + "_" + offsetX + "_" + offsetY + "_" + offsetZ + ".swc";
+                                if (!FileHelper.storeFile(storePath, filename, fileContent)) {
+                                    downloadSwcResult.postValue(new Result.Error(new Exception("Fail to store image file !")));
+                                }
+                                downloadSwcResult.postValue(new Result.Success(storePath + "/" + filename));
+                                response.body().close();
+                                response.close();
+                            }else{
+                                downloadSwcResult.postValue(new Result.Error(new Exception("Response from server is null when download swc !")));
+                            }
+                        }else{
+                            downloadSwcResult.postValue(new Result.Error(new Exception("Response from server is error when download swc !")));
                         }
-                        response.body().close();
-                        response.close();
+                    } catch (Exception e) {
+                        downloadSwcResult.postValue(new Result.Error(new Exception("Fail to download swc file !")));
+                        e.printStackTrace();
                     }
+
                 }
             });
         } catch (Exception e) {
+            downloadSwcResult.postValue(new Result.Error(new IOException("Check the network please !", e)));
             e.printStackTrace();
         }
     }
@@ -131,12 +149,12 @@ public class QualityInspectionDataSource {
 
     public void UpdateCheckResult(int arborId,String arborName,int somaType,JSONArray insertList,JSONArray deleteList,String name,String image){
         try{
+            Log.e(TAG,"start updateCheckResult");
             JSONObject userInfo = new JSONObject().put("name", InfoCache.getAccount()).put("passwd", InfoCache.getToken());
             HttpUtilsQualityInspection.UpdateCheckResultWithOkHttp(userInfo, arborId,arborName,somaType, insertList, deleteList, name, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     updateArborMarkerResult.postValue(new Result.Error(new Exception("Connect failed when upload marker list !")));
-
                 }
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
@@ -162,90 +180,38 @@ public class QualityInspectionDataSource {
 
     public void getArborMarkerList(String arborName){
         try{
+
             JSONObject userInfo = new JSONObject().put("name", InfoCache.getAccount()).put("passwd", InfoCache.getToken());
             HttpUtilsQualityInspection.getArborMarkerListWithOkHttp(userInfo, arborName, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     arborMarkerListResult.postValue(new Result.Error(new Exception("Connect failed when get arbor list!")));
                 }
-
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
-
+                    int responseCode = response.code();
+                    if(responseCode == 200){
+                        responseData = response.body().string();
+                        Log.e(TAG,"responseData_getArborMarker"+responseData);
+                        try{
+                            JSONArray arborMarkerList = new JSONArray(responseData);
+                            MarkerList markerList = MarkerList.parseToJSONArray(arborMarkerList);
+                            arborMarkerListResult.postValue(new Result.Success<MarkerList>(markerList));
+                        } catch (Exception e) {
+                            arborMarkerListResult.postValue(new Result.Error(new Exception("Fail to parse arbor marker list !")));
+                            e.printStackTrace();
+                        }
+                        response.body().close();
+                    }else{
+                        arborMarkerListResult.postValue(new Result.Error(new Exception("Fail to get arbor marker list !")));
+                    }
                 }
             });
 
         } catch (Exception e) {
+            arborMarkerListResult.postValue(new Result.Error(new IOException("Check the network please !", e)));
             e.printStackTrace();
         }
     }
 
-
-
-
-
-//    public void getSomaList(String image, int x, int y, int z, int size){
-//        try {
-//            JSONObject userInfo = new JSONObject().put("name", InfoCache.getAccount()).put("passwd", InfoCache.getToken());
-//            HttpUtilsSoma.getSomaListWithOkHttp(userInfo, image, x, y, z, size, new Callback() {
-//                @Override
-//                public void onFailure(Call call, IOException e) {
-//                    somaListResult.postValue(new Result.Error(new Exception("Connect failed when get soma list !")));
-//                }
-//
-//                @Override
-//                public void onResponse(Call call, Response response) throws IOException {
-//                    int responseCode = response.code();
-//                    if (responseCode == 200) {
-//                        // process response
-//                        responseData = response.body().string();
-//                        Log.e(TAG, "responseData: " + responseData);
-//                        try {
-//                            JSONArray somaList = new JSONArray(responseData);
-//                            MarkerList markerList = MarkerList.parseFromJSONArray(somaList);
-//                            somaListResult.postValue(new Result.Success<MarkerList>(markerList));
-//                        } catch (JSONException e) {
-//                            somaListResult.postValue(new Result.Error(new Exception("Fail to parse soma list !")));
-//                            e.printStackTrace();
-//                        }
-//                        response.body().close();
-//                        response.close();
-//                    } else {
-//                        somaListResult.postValue(new Result.Error(new Exception("Fail to get soma list !")));
-//                    }
-//                }
-//            });
-//        } catch (Exception exception) {
-//            somaListResult.postValue(new Result.Error(new IOException("Check the network please !", exception)));
-//        }
-//    }
-//
-//    public void updateSomaList(String image, int locationId, String username, JSONArray insertSomaList, JSONArray deleteSomaList){
-//        try {
-//            Log.e(TAG,"start updateSomaList");
-//            JSONObject userInfo = new JSONObject().put("name", InfoCache.getAccount()).put("passwd", InfoCache.getToken());
-//            HttpUtilsSoma.updateSomaListWithOkHttp(userInfo, locationId, insertSomaList, deleteSomaList, username, image, new Callback() {
-//                @Override
-//                public void onFailure(Call call, IOException e) {
-//                    updateSomaResult.postValue(new Result.Error(new Exception("Connect failed when upload marker list !")));
-//                }
-//
-//                @Override
-//                public void onResponse(Call call, Response response) throws IOException {
-//                    Log.e(TAG,"receive response");
-//                    int responseCode = response.code();
-//                    if (responseCode == 200) {
-//                        // process response
-//                        updateSomaResult.postValue(new Result.Success<String>(UPLOAD_SUCCESSFULLY));
-//                    } else {
-//                        Log.e(TAG,"response: " + response.body().string());
-//                        updateSomaResult.postValue(new Result.Error(new Exception("Fail to upload marker list !")));
-//                    }
-//                    response.close();
-//                }
-//            });
-//        } catch (Exception exception) {
-//            updateSomaResult.postValue(new Result.Error(new IOException("Check the network please !", exception)));
-//        }
-//    }
 }
