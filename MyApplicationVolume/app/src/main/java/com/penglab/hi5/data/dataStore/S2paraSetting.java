d309c466bfdd9f279b58c78c50bbb62e9a0fb80b
DILIU1 <1041763279@QQ.COM>
2022-05-22 21:47:05 +0800

    Added the ui to label data
    Added the ui to label data

--- MyApplicationVolume/app/src/main/java/com/penglab/hi5/data/dataStore/S2paraSetting.java
+++ MyApplicationVolume/app/src/main/java/com/penglab/hi5/data/dataStore/S2paraSetting.java
@@ -26,8 +26,36 @@ public class S2paraSetting {
 
     }
 
+    public void setTag(int img_score, int swc_score,String id ,String name,String note){
+        editor = pref.edit();
+
+        editor.putInt("image_quality_score",img_score);
+        editor.putInt("swc_quality_score",swc_score);
+        editor.putString("user_id",id);
+        editor.putString("file_name",name);
+        editor.putString("notes",note);
+        editor.apply();
+
+    }
+
     public boolean getConnect_ServerMode(){
         return pref.getBoolean("Connect_ServerMode",false);
+    }
+
+    public int getImage_quality_score(){
+        return pref.getInt("image_quality_score",0);
+    }
+    public int getSwc_quality_score(){
+        return pref.getInt("swc_quality_score",0);
+    }
+    public String getUser_id(){
+        return pref.getString("user_id", String.valueOf(0));
+    }
+    public String getFile_name(){
+        return pref.getString("file_name",String.valueOf(0));
+    }
+    public String getNotes(){
+        return pref.getString("notes",String.valueOf(0));
     }
 
     public boolean getConnect_ScopeMode(){
